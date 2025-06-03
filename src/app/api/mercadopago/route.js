20dd22c685d6c26d8ddefb8e08a2eb88f3fdf1af
Widido1 <sdg-1995@protonmail.com>
2025-06-03 20:40:57 -0300

    test webhooks

--- src/app/api/mercadopago/route.js
+++ src/app/api/mercadopago/route.js
@@ -2,11 +2,10 @@ import { getPaymentStatus } from "@/api";
 
 export async function POST(request) {
     const body = await request.json();
-    await getPaymentStatus(body.id);
-    return new Response(JSON.stringify({ message: "Payment status checked successfully" }), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+    if (body.type === "payment") {
+        const payment = await getPaymentStatus(body.data.id);
+        console.log("Payment status: ", payment);
+        // Process payment status
+    }
+    return new Response(null, { status: 200 });
 }
